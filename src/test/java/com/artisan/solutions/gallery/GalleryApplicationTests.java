35cb30e0c5d07248634429b6a9f147442f5379d0
s3ld0n <ashavrn@protonmail.com>
2020-05-13 12:16:19 +0300

    add pom.xml and src dir

---
+++ src/test/java/com/artisan/solutions/gallery/GalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artisan.solutions.gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
